c07fe4384abcfa503ece86358e3385860bb7e2aa
Caio Ribeiro <cacoribeiro@pop-os.localdomain>
2020-08-20 19:12:30 -0300

    Updates

---
+++ precache-manifest.8783787ea813c0358abf8bef6c430fd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5440877953e971a029ba1f936ca07073",
+    "url": "/ShowRoom/index.html"
+  },
+  {
+    "revision": "8af1844e02a1312b1b1e",
+    "url": "/ShowRoom/static/css/main.84f9e4b9.chunk.css"
+  },
+  {
+    "revision": "4b81e0bea1bfacc3ae15",
+    "url": "/ShowRoom/static/js/2.5a1e501e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ShowRoom/static/js/2.5a1e501e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af1844e02a1312b1b1e",
+    "url": "/ShowRoom/static/js/main.ac4cb48d.chunk.js"
+  },
+  {
+    "revision": "e2bd13f73566df7966db",
+    "url": "/ShowRoom/static/js/runtime-main.65e29cce.js"
+  }
+]);
